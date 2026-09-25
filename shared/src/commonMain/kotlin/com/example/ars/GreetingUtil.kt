4466fdbaab5c22d64151a9394543513d7db3db6b
niicomg <nicmar05@ucm.es>
2026-09-25 15:15:47 +0200

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/example/ars/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.ars
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/jvmTest/kotlin/com/example/ars/SharedLogicDesktopTest.kt
@@ -0,0 +1,12 @@
+package com.example.ars
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedLogicDesktopTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
